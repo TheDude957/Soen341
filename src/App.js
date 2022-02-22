a1d993e88967a83a91e549050518dd7ec30c8282
Julian Lussier <julianlussier@gmail.com>
2022-02-21 23:02:22 -0500

    created productcard component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import Test1 from "./test1";
 import Test2 from "./test2";
 import HomePage from "./Pages/HomePage";
 import Navbar from "./Components/Navbar";
+import LoginPage from "./Pages/LoginPage";
+import Footer from "./Components/Footer";
+import Cart from "./Components/Cart";
+
+
 import React from "react";
 function App() {
   return (
@@ -12,7 +17,10 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/test2" element={<Test2 />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/cart" element={<Cart />} />
         </Routes>
+        <Footer />
       </Router>
     </>
   );
--- src/Components/ProductCard.js
+++ src/Components/ProductCard.js
@@ -1,30 +1,44 @@
-import {firebase} from "../Setup.js";
-let products = [];
-function ProductCard() {
-    // reference to database containing products
-  var productRef = firebase.database().ref("/product");
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import bert from "../Images/bert_n_ernie_socks.jpg"
 
-  productRef.once("value", function (snapshot) {
 
-    snapshot.forEach(function (childSnapshot) {
-      var key = childSnapshot.key;
-      var data = childSnapshot.val();
-      products.push({
-        key: key,
-        name: data.name,
-        id: data.id,
-        picture: data.picture,
-        price: data.price,
-        category: data.category
-      });
-    });
-  });
+function ProductCard(props) {
+  
+ 
 
-  return (<div>
-      Hello it's me!
+  return (
+
+     <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        height="140"
+        image = {require ("../Images/bert_n_ernie_socks.jpg")}
+        alt="bert and ernie"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+        {props.name}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {props.id}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">{props.price}</Button>
+        <Button size="small">{props.category}</Button>
+      </CardActions>
+    </Card>
       
       
-      </div>);
+      );
 }
 
 export default ProductCard;
+
+
