5b5117d0560fa9a17689e64e4a2d1a8d9bbb59be
Liliane Nguemtchouang <liliane.duchelle@gmail.com>
2022-02-23 18:11:18 -0500

    Created a search bar for the landing page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 import Test1 from './test1';
 import Test2 from './test2';
 import Navbar from './Components/Navbar.js'
+import ProductSearch from './Components/ProductSearch';
 function App() {
   return (
     <>
     <Router>
       <Navbar/>
+      <ProductSearch/>
       <Routes>
         <Route path ="/" element ={<Test1/>}/>
         <Route path="/test2" element = {<Test2/>}/>
