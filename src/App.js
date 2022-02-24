8437c8b7572cbf77d10f5c4ab2e7960a449aa1d8
Titouan Sablé <76408538+Resident-png@users.noreply.github.com>
2022-02-23 21:15:08 -0500

    Update App.js
    Some changes were deleted after merging another branch

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Navbar from "./Components/Navbar";
 import LoginPage from "./Pages/LoginPage";
 import Footer from "./Components/Footer";
 import Cart from "./Components/Cart";
+import ProductSearch from "./Components/ProductSearch";
 
 
 import React from "react";
@@ -14,6 +15,7 @@ function App() {
     <>
       <Router>
         <Navbar />
+        <ProductSearch />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/test2" element={<Test2 />} />
