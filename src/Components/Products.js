ed3fba69a552fc37fbf55869899db9bc7c1569fe
tenzabest <tenzabest2001@gmail.com>
2022-03-01 09:46:13 -0500

    Display products #81

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,61 +1,33 @@
-import { DownloadDone } from "@mui/icons-material";
 import { useEffect, useState } from "react";
-import { getProducts, searchProduct } from "../firebaseService";
+import { getProducts } from "../firebaseService";
 import ProductCard from "./ProductCard";
 
 function Products(props) {
   const [getProduct, setProducts] = useState([]);
-  const [isSearched, setIsSearched] = useState(false);
-
-
 
   useEffect(() => {
-    
-    console.log("is Search " + isSearched + props.searchValue);
-    if (!isSearched) {
-      getProducts().then((values) => {
-        values.forEach((value) => {
-          setProducts((oldArray) => [...oldArray, value]);
-        });
+    getProducts().then((products) => {
+      products.forEach((product) => {
+        setProducts((prev) => [...prev, product]);
       });
-    }
-    else{
-        searchProduct(props.searchValue).then((values) => {
-          values.forEach((value) => {
-            setProducts((oldArray) => [...oldArray, value]);
-          });
-        });
-
-    }
-    return () => {
-      if (props.searchValue === "") {
-        setIsSearched(false);
-      } else {
-        setIsSearched(true);
-      }
-      setProducts([]);
-  }
-
-  }, [props.searchValue]);
-
+    });
+  }, []);
 
   return (
-    <>
-      <ul className="list-grid">
-        {getProduct.map((prod) => {
-          return (
-            <li class="list-in-grid">
-              <ProductCard
-                price={prod.price}
-                id={prod.id}
-                category={prod.category}
-                name={prod.name}
-              ></ProductCard>
-            </li>
-          );
-        })}
-      </ul>
-    </>
+    <div className="products">
+      {getProduct.map((product) => {
+        return (
+          <ProductCard
+            price={product.price}
+            id={product.id}
+            category={product.category}
+            name={product.name}
+            description={product.description}
+            picture={product.picture}
+          ></ProductCard>
+        );
+      })}
+    </div>
   );
 }
 export default Products;
