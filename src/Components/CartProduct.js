c283c59150524787b2e230ba4ad3182951077f1f
tenzabest <tenzabest2001@gmail.com>
2022-03-20 09:44:23 -0400

    Add and remove item from Cart #94

--- src/Components/CartProduct.js
+++ src/Components/CartProduct.js
@@ -1,17 +1,22 @@
-import * as React from "react";
+import { useEffect, useState } from "react";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
 import "../CSS/ProductCard.css";
-import { AddItemToCart } from "../firebaseService";
-function ProductCard(props) {
-  async function addToCart() {
-    await AddItemToCart(props.id);
-  }
+import { GetCurrentUserCart, RemoveItemFromCart } from "../firebaseService";
 
+function CartProduct(props) {
+  const [quantity, setQuantity] = useState(1);
+
+  useEffect(() => {}, []);
+  async function removeItem() {
+    await RemoveItemFromCart(props.id);
+    props.setProductsCart(await GetCurrentUserCart());
+  }
   return (
     <Card className="product">
       <CardMedia
@@ -27,18 +32,35 @@ function ProductCard(props) {
         </Typography>
         <Typography className="product-text price">${props.price}</Typography>
       </CardContent>
+      <span className="quantity">Quantity</span>
+      <TextField
+        id="outlined-number"
+        type="number"
+        onChange={(number) => {
+          setQuantity(!isNaN(number.target.value) ? number.target.value : "");
+        }}
+        value={quantity}
+        size="small"
+        // className="product-text quantity-box"
+        InputLabelProps={{
+          shrink: true,
+        }}
+        inputProps={{ min: 1, inputMode: "numeric" }}
+        // onkeydown="return false;"
+      />
+
       <CardActions>
         <Button
           color="primary"
           variant="contained"
           fullWidth
-          onClick={addToCart}
+          onClick={removeItem}
         >
-          ADD TO CART
+          Remove
         </Button>
       </CardActions>
     </Card>
   );
 }
 
-export default ProductCard;
+export default CartProduct;
