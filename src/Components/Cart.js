c5b790361ab50a64d5ca6ac38334ad807046e844
tenzabest <tenzabest2001@gmail.com>
2022-03-20 09:42:57 -0400

    Display items in Cart #80

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,8 +1,51 @@
-function Cart(){
+import CartProduct from "./CartProduct";
+import { useEffect, useState } from "react";
+import { getProducts, GetCurrentUserCart } from "../firebaseService";
+function Cart() {
+  const [getProduct, setProducts] = useState([]);
 
-    return(
-        <div>Wow, such empty!</div>
-    );
+  const [productsCart, setProductsCart] = useState([]);
+
+  async function getUserCart() {
+    await GetCurrentUserCart().then((cartItems) => {
+      if (cartItems !== undefined) {
+        setProductsCart(Object.values(cartItems));
+        console.log("CartItems: ", Object.values(cartItems));
+      }
+    });
+  }
+
+  useEffect(() => {
+    getProducts().then((products) => {
+      products.forEach(async (product) => {
+        setProducts((prev) => [...prev, product]);
+        console.log("Value of products : ", products);
+      });
+    });
+  }, []);
+
+  useEffect(() => {
+    getUserCart();
+  }, []);
+  return (
+    <div className="products">
+      {getProduct
+        .filter((product) => productsCart.includes(product.id))
+        .map((product) => {
+          return (
+            <CartProduct
+              price={product.price}
+              id={product.id}
+              category={product.category}
+              name={product.name}
+              description={product.description}
+              picture={product.picture}
+              setProductsCart={setProductsCart}
+            ></CartProduct>
+          );
+        })}
+    </div>
+  );
 }
 
 export default Cart;
